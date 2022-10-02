c808a5e1fa6328c451004ea8ac2670e39a0fd223
cw-barry <barry@clarusway.com>
2022-10-01 22:01:16 -0400

    update cards

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Picture from '../components/Picture';
+import data from '../data';
+
+const Homepage = () => {
+  console.log(data);
+  return (
+    <div style={{ minHeight: '100vh' }}>
+      <h1>Albums</h1>
+      <div className="pictures">
+        {data?.map((d) => {
+          return <Picture data={d} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
