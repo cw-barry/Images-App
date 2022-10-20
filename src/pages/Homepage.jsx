3aeffe99717a418d0ca391d57c217ad808fcc939
cw-barry <barry@clarusway.com>
2022-10-19 21:20:02 -0400

    add api

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Picture from '../components/Picture';
-import data from '../helper/data';
+// import data from '../helper/data';
 
 const Homepage = () => {
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    fetch('https://cwbarry.pythonanywhere.com/image/')
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setImages(data.images);
+      });
+  }, []);
   return (
     <div style={{ minHeight: '100vh' }}>
       <h1>Image Gallery</h1>
       <div className="pictures">
-        {data?.map((d) => (
-          <Picture data={d} />
+        {images?.map((item, index) => (
+          <Picture key={index} data={item} />
         ))}
       </div>
     </div>
